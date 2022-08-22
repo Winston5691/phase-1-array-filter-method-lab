963a0dbdcbb1482477c48c43cfffb36f756ecde6
Wiston <winston.langat@student.moringaschool.com>
2022-08-22 11:20:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+let findMatching = (listOfDrivers, searchedDriver) => {
+    return listOfDrivers.filter((driverName) => {
+      return driverName.toLowerCase() === searchedDriver.toLowerCase();
+    });
+  };
+  
+  let fuzzyMatch = (listOfDrivers, matchingLetters) => {
+    return listOfDrivers.filter((driverName) => {
+      return driverName.substring(0, 2) === matchingLetters;
+    });
+  };
+  
+  let matchName = (listOfDrivers, searchedDriver) => {
+    return listOfDrivers.filter((driverName) => {
+      return driverName.name === searchedDriver;
+    });
+  };
